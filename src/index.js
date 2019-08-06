2abccf8a77783a05f841610108b19dbfcdf9fc81
yue.jin <yue.jin@bianlifeng.com>
2019-08-06 11:48:03 +0800

    newha2

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import qs from 'query-string';
 import { cube } from './math';
 import txtStr from '$assets/test.txt';
 
-哥斯拉2
-
 function throttle(fn, threshhold = 3000) {
   let timeout;
   let isNotAllow = false;
